f53e92b7693c67ac283569e43b900f9ced9436d5
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-08-06 19:41:19 +0300

    Updated file

--- 02/main_02.js
+++ 02/main_02.js
@@ -26,9 +26,20 @@ const students = [
 ];
 
 //const names = ["Bob", "Alex", "Nick", "John"];
+//const updatedStudents = [{
+//  name: "Bob",
+//  age: 22,
+//  isMarried: true,
+//    scores: 85,
+//    isStudent: true
+//  },]
 
 //const getNames = students.map((st) => st.name);
 //console.log(getNames);
+
+//Функции-преобразователи:
+const getValueForResult = (el) => el.name;
+const addNewProperty = (el) => ({ ...el, isStudent: true });
 
 const getNamesWithoutMethods = (array) => {
   const result = [];
@@ -41,4 +52,25 @@ const getNamesWithoutMethods = (array) => {
   return result;
 };
 
+const getUpdatedStudents = (array) => {
+  let result = [];
+  const addNewProperty = (el) => ({ ...el, isStudent: true });
+  for (let index = 0; index < array.length; index++) {
+    //  result[index] = { ...array[index], isStudent: true };
+    result[index] = addNewProperty(array[index]);
+  }
+  return result;
+};
+
+const getMappedArray = (array, func) => {
+  let result = [];
+  for (let index = 0; index < array.length; index++) {
+    result[index] = func(array[index]);
+  }
+  return result;
+};
+
 console.log(getNamesWithoutMethods(students));
+console.log(getUpdatedStudents(students));
+console.log(getMappedArray(students, getValueForResult));
+console.log(getMappedArray(students, addNewProperty));
