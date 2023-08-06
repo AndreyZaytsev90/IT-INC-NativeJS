5937c749dc11346ac73ff396145ef0f2466cef6a
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-08-06 21:39:41 +0300

    Updated file

--- 02/main_02.js
+++ 02/main_02.js
@@ -70,7 +70,49 @@ const getMappedArray = (array, func) => {
   return result;
 };
 
+const getFilteredArray = (array, funcCond) => {
+  const result = [];
+  for (let index = 0; index < array.length; index++) {
+    if (funcCond(array[index]) === true) {
+      result[result.length] = array[index];
+    }
+  }
+  return result;
+};
+
+const pushElToArray = (array, el) => {
+  array[array.length] = el;
+  return array.length;
+};
+
+const isIncludes = (array, el) => {
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === el) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const findEl = (array, searchCondFunc) => {
+  for (let index = 0; index < array.length; index++) {
+    if (searchCondFunc(array[index]) === true) {
+      return array[index];
+    }
+  }
+  return false;
+};
+
 console.log(getNamesWithoutMethods(students));
 console.log(getUpdatedStudents(students));
 console.log(getMappedArray(students, getValueForResult));
 console.log(getMappedArray(students, addNewProperty));
+
+console.log(students.map(getValueForResult));
+console.log(students.map(addNewProperty));
+//console.log(students.map((el) => el.name));
+
+console.log(getFilteredArray(students, (el) => el.age > 20));
+console.log(students.filter((el) => el.age > 20));
+
+console.log(findEl(students, (el) => el.name === "Bob"));
