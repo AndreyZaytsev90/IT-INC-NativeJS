36f8e7ae7c425537af26449e2a7844ea48e2e56f
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-08-17 22:46:12 +0300

    Updated file

--- 03/main_03.js
+++ 03/main_03.js
@@ -4,3 +4,25 @@
 // При наступлении события браузер генерирует объект типа Event (new Event())
 // {...} => event, e, ev
 // При наступлении события, генерируется объект е => fn(e) и он передается в функцию в качестве параметра.
+
+const small = document.getElementById("small");
+
+function handler() {
+  alert("you click small");
+}
+function handler2() {
+  alert("you click small2");
+}
+
+small.onclick = handler;
+small.onclick = handler2; // handler перезатерся на handler2
+small.onclick = null;
+
+/*small.onclick = () => {
+  handler();
+  handler2();
+};
+*/
+
+small.addEventListener("click", handler);
+small.addEventListener("click", handler2); // функции вызываются по очереди. Они не затераются
